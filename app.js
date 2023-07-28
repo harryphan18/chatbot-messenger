c96d55860857a27cad6c817fb0da9af8fc4bb0cb
Huy Phan <huyphan5218@gmail.com>
2023-07-28 18:03:41 +0700

    connect to facebook dev

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var webRoutes = require("./src/routes");
 require('dotenv').config();
 const app = express();
 
-viewEngine(app);
-webRoutes(app);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+viewEngine(app);
+webRoutes(app);
 
 app.listen(process.env.PORT, (error) => {
     console.log(`App is listening at port ${process.env.PORT}...`);
